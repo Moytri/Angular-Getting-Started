e0a4e72332f7541d12267139d1784fedbf035a57
moytri <moytritopa@gmail.com>
2018-06-29 14:25:09 -0700

    add comment for decorator and metadata

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+//Always need to import the Component symbol from the core angular library
+//@Component is the decorator function
+//This decorator function specifies Angular metadata for the Component
+//CLI decorator has three metadata properties - selector, templateUrl, styleUrls
 @Component({
-  selector: 'app-heroes',
+  selector: 'app-heroes',       //Component's CSS element selector
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
 
+export class HeroesComponent implements OnInit {
+  hero = 'Windstrom';
   constructor() { }
 
   ngOnInit() {
